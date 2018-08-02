85ffedee3c6a2ec2e4b51468e88a08ca83eb97ba
Ivan Simatupang <ivansmtg13@gmail.com>
2018-08-02 16:12:34 +0700

    Update.

--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-import boto3
-idServer = os.environ['ids']
-idMonitoring = os.environ['id']
-def handler(event, context):
-    region = 'ap-northeast-1'
-    ec2 = boto3.client('ec2', region_name=region)
-    ec2.stop_instances(InstanceIds=[idServer,idMonitoring], DryRun=False)
-    print "Stopping your instance...."
